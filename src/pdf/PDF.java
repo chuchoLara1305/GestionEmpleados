06e0421cdf8d5879e88a18254350663978079614
Chucho Lara <112294881+chuchoLara1508@users.noreply.github.com>
2023-06-01 07:09:12 -0600

    Update PDF.java

--- src/pdf/PDF.java
+++ src/pdf/PDF.java
@@ -14,41 +14,43 @@ import com.itextpdf.text.pdf.draw.LineSeparator;
 import paquete.Empleado;
 
 public class PDF implements Impresion{
- private String titulo;
- private String asunto;
- private String autor;
- private String creador;
- 
- public PDF(String titulo, String asunto, String autor, String creador) {
-	this.titulo = titulo;
-	this.asunto = asunto;
-	this.autor=autor;
-	this.creador = creador;
- }
+	private String titulo;
+	 private String asunto;
+	 private String autor;
+	 private String creador;
+	 private String nombreArchivo;
+	 
+	 public PDF(String titulo, String asunto, String autor, String creador,String nombreArchivo) {
+		this.titulo = titulo;
+		this.asunto = asunto;
+		this.autor=autor;
+		this.creador = creador;
+		this.nombreArchivo=nombreArchivo;
+	 }
 
- @Override
- public void imprimir(Empleado emp) {
-	 try {
-		 Document documento=new Document();
-		 PdfWriter.getInstance(documento,new FileOutputStream("información.pdf"));
-		 documento.open();
-		 documento.addTitle(titulo);
-         documento.addSubject(asunto);
-         documento.addAuthor(autor);
-         documento.addCreator(creador);
-         documento.add(new Paragraph("Información de empleados"));
-         documento.add(new Paragraph("Nombre: "+emp.getNombre()));
-         documento.add(new Paragraph("Sueldo: "+emp.getSueldo()));
-         documento.add(new Paragraph("Afiliación: "+emp.getAfiliacion()));
-         documento.add(new Paragraph("Años de servicio: "+emp.getAniosServicio()));
-         documento.close();
+	 @Override
+	 public void imprimir(Empleado emp) {
+		 try {
+			 Document documento=new Document();
+			 PdfWriter.getInstance(documento,new FileOutputStream(nombreArchivo));
+			 documento.open();
+			 documento.addTitle(titulo);
+	         documento.addSubject(asunto);
+	         documento.addAuthor(autor);
+	         documento.addCreator(creador);
+	         documento.add(new Paragraph("Información de empleados"));
+	         documento.add(new Paragraph("Nombre: "+emp.getNombre()));
+	         documento.add(new Paragraph("Sueldo: "+emp.getSueldo()));
+	         documento.add(new Paragraph("Afiliación: "+emp.getAfiliacion()));
+	         documento.add(new Paragraph("Años de servicio: "+emp.getAñosDeServicio()));
+	         documento.add(new Paragraph("Teléfono: "+emp.getTelefono()));
+	         documento.add(new Paragraph("Correo: "+emp.getCorreo()));
+	         documento.add(new Paragraph("Edad: "+emp.getEdad()));
+	         documento.close();
+		 }
+		 catch(Exception err) {
+			 System.out.println("Error de generación de archivo: "+err.getMessage());
+		 }
+		
 	 }
-	 catch(Exception err) {
-		 System.out.println("Error de generación de archivo: "+err.getMessage());
-	 }
-	
- }
- 
- 
- 
 }
