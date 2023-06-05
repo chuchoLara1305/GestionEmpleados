ab1870c0dd5fadae0a6145294599bc3874897234
Paola Hernandez <134995434+paoHernandez05@users.noreply.github.com>
2023-06-04 22:27:24 -0600

    Update Database.java

--- src/graficacion/src/graficacion/Database.java
+++ src/graficacion/src/graficacion/Database.java
@@ -5,31 +5,6 @@ import java.sql.*;
 
 public class Database {
 	
-	public ArrayList<Empleado> ObtenerFuenteDatosManual(){
-		
-		//creacion de instancias manual de la clase "Empleado"(prueba)
-		Empleado empleado01 = new Empleado("Arturo", 33, 500000f); 	
-		Empleado empleado02 = new Empleado("Taylor", 22, 130131f);
-		Empleado empleado03 = new Empleado("Paola", 20, 5455f);
-		Empleado empleado04 = new Empleado("Daniel", 55, 45513f);
-		Empleado empleado05 = new Empleado("Harry", 28, 400000.13f);
-		Empleado empleado06 = new Empleado("George", 35, 120300.25f);
-		Empleado empleado07 = new Empleado("Charlotte", 39, 400001f);
-		
-		//creacion de la lista de la clase tipo "Empleado"
-		ArrayList<Empleado> empleados = new ArrayList<Empleado>();
-		empleados.add(empleado01);
-		empleados.add(empleado02);
-		empleados.add(empleado03);
-		empleados.add(empleado04);
-		empleados.add(empleado05);
-		empleados.add(empleado06);
-		empleados.add(empleado07);
-		
-		return empleados;
-		
-	}
-	
 	public ArrayList<Empleado> ObtenerFuenteDatosMySQL(){
 		
 		ArrayList<Empleado> empleados = new ArrayList<Empleado>();
