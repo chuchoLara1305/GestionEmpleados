ad918349885a5ea15309e12c3e15703f854827af
Paola Hernandez <134995434+paoHernandez05@users.noreply.github.com>
2023-06-04 22:32:57 -0600

    Update Main.java

--- src/graficacion/src/graficacion/Main.java
+++ src/graficacion/src/graficacion/Main.java
@@ -5,14 +5,39 @@ public class Main{
 	
 	public static void main(String[] args) {
 		
+				case 4:
+				crearSubMenuGraficacion();
+				graficarEmpleados();
+				break;
+
+	//menu de graficacion 
+	public static void crearSubMenuGraficacion() {
+		Menu menu = new Menu("Graficar datos de empleados", '*');
+		menu.agregarOpcion((short)1, "Graficar edades", Colores.magenta);
+		menu.agregarOpcion((short)2, "Graficar sueldos", Colores.amarillo);
+		menu.agregarOpcion((short)3, "Graficar edades y sueldos", Colores.azul);
+		menu.desplegar();
+	}
+	public static void graficarEmpleados() {
+		Scanner read = new Scanner(System.in);
+		System.out.println("Seleccione la opcion:");
+		int opcion = read.nextInt();
+		read.nextLine();
 		Database db = new Database();
-		
-		ArrayList<Empleado> empleados = db.ObtenerFuenteDatosManual();
+		ArrayList<Empleado> empleados = db.ObtenerFuenteDatosMySQL();
 		Graficar miGrafica = new Graficar();
-		miGrafica.graficarEdad(empleados);
-		miGrafica.graficarSueldo(empleados);
-		miGrafica.graficarTodo(empleados);
-		
+		switch(opcion) {
+		case 1:
+			miGrafica.graficarEdad(empleados);
+			break;
+		case 2:
+			miGrafica.graficarSueldo(empleados);
+			break;
+		case 3:
+			miGrafica.graficarTodo(empleados);
+			break;
+		}
+	}	
 	}
 
 }
